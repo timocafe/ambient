f8cca1ae234e967fb46831404066985b5bf2708b
Alex Kosenkov <alex.kosenkov@gmail.com>
2016-03-16 17:03:23 +0100

    convenience decomp for mp_gemm_simple

--- examples/mp_gemm_simple.cpp
+++ examples/mp_gemm_simple.cpp
@@ -2,16 +2,25 @@
 #include "ambient/container/numeric/matrix.hpp"
 #include "utils/timer.hpp"
 
-#define N AMBIENT_DEFAULT_IB*4 
+#define N AMBIENT_DEFAULT_IB*64
 
-ambient::scope::const_iterator where(int i, int j){
-    static int nq = ambient::scope::size();
-    static int np = 1;
-    return (ambient::scope::begin() + (i % np) * nq + j % nq);
-}
+struct Decomposition {
+    void init(int np, int nq){
+        np_ = np; nq_ = nq;
+        std::cout << "Proc grid: " << np << "x" << nq << "\n";
+    }
+    ambient::scope::const_iterator operator()(int i, int j){
+        return (ambient::scope::begin() + (i % np_) * nq_ + j % nq_);
+    }
+    int np_;
+    int nq_;
+} where;
 
-int main(){
-    ambient::tiles< ambient::matrix<double> > a(N,N), b(N,N), c(N,N);
+int main(int argc, char* argv[]){
+    using matrix = ambient::matrix<double>;
+    if(argc > 2) where.init(std::stoi(argv[1]), std::stoi(argv[2]));
+    else where.init(ambient::scope::size(), 1);
+    ambient::tiles< matrix > a(N,N), b(N,N), c(N,N);
 
     for(int i = 0; i < a.mt; i++)
     for(int j = 0; j < a.nt; j++){
